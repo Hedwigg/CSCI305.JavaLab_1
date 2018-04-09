8ab8e854c375fa9925016d4f461d2afbb3f805dc
Joel Lechman <Hedwigg01@gmail.com>
2018-04-08 18:29:06 -0600

    realized that LastPlayBot is not working at all...

--- src/csci305/javalab/Player.java
+++ src/csci305/javalab/Player.java
@@ -24,3 +24,8 @@ public abstract class Player {
 	public abstract Element Play();
 	
 }
+
+//todo
+//variable to hold last play (which is updated in every player's version of play()
+//a get method for this variable so that lastplaybot can find the information.
+//somehow give lastplaybot the ability to figure out it's opponent.
